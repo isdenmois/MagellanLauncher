3d240378b8d7e753874111792f822cba366f6fc3
Denis Moiseev <isdenmois@gmail.com>
2015-06-20 02:08:45 +0300

    Added method for create app list

--- app/src/main/java/isden/mois/magellanlauncher/IsdenTools.java
+++ app/src/main/java/isden/mois/magellanlauncher/IsdenTools.java
@@ -1,6 +1,14 @@
 package isden.mois.magellanlauncher;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.webkit.MimeTypeMap;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class IsdenTools {
     public static String get_mime_by_filename(String filename) {
@@ -39,5 +47,39 @@ public class IsdenTools {
             result = time + "ч " + result;
         }
         return result;
+    }
+
+    /**
+     * Creates sorted list with applications.
+     */
+    public static List<Application> createAppList(Context ctx) {
+        String selfName = ctx.getPackageName();
+        PackageManager pm = ctx.getPackageManager();
+        List<Application> packages = new ArrayList<Application>();
+
+        Intent componentSearchIntent = new Intent();
+        componentSearchIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        componentSearchIntent.setAction(Intent.ACTION_MAIN);
+        List<ResolveInfo> ril = pm.queryIntentActivities(componentSearchIntent, 0);
+
+        for (ResolveInfo ri : ril) {
+            if (ri.activityInfo != null) {
+                Application app = new Application();
+                app.packageName = ri.activityInfo.packageName;
+                try {
+                    if (ri.activityInfo.labelRes != 0) {
+                        app.name = (String) ri.activityInfo.loadLabel(pm);
+                    } else {
+                        app.name = (String) ri.loadLabel(pm);
+                    }
+                } catch (Exception e) { }
+                if (app.packageName != null && !app.packageName.equals(selfName)) {
+                    packages.add(app);
+                }
+            }
+        }
+        Collections.sort(packages, new AppComparator());
+
+        return packages;
     }
 }
