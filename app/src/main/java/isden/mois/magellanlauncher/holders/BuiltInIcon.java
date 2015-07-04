6b575b2fb93abef3a01289bc3175c0212284f50c
isdenmois <isdenmois@gmail.com>
2015-07-04 16:27:12 +0300

    Load image info from xml

--- app/src/main/java/isden/mois/magellanlauncher/holders/BuiltInIcon.java
+++ app/src/main/java/isden/mois/magellanlauncher/holders/BuiltInIcon.java
@@ -1,26 +1,35 @@
 package isden.mois.magellanlauncher.holders;
 
 import android.widget.ImageView;
+import isden.mois.magellanlauncher.MainActivity;
 
 /**
  * Created by isden on 28.06.15.
  */
 public class BuiltInIcon implements IIcon {
-    int image;
+    private int index;
 
     public BuiltInIcon(int image) {
-        this.image = image;
+        if (image >= 0 && image < MainActivity.builtInImages.length()) {
+            this.index = image;
+        }
+        else {
+            this.index = 0;
+        }
     }
 
     @Override
     public void setIcon(ImageView iw) {
-        if (iw != null) {
-            iw.setImageResource(image);
+        if (iw != null && index >= 0 && index < MainActivity.builtInImages.length()) {
+            int image = MainActivity.builtInImages.getResourceId(index, -1);
+            if (index >= 0) {
+                iw.setImageResource(image);
+            }
         }
     }
 
     @Override
     public String getString() {
-        return String.valueOf(image);
+        return String.valueOf(index);
     }
 }
