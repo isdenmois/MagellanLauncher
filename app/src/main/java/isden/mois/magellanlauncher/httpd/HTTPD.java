fe68d5e3fb9c0ab6633cda5f5e24c97e1365dad2
D. Moiseev <d.moiseev@initlab.ru>
2017-01-05 18:20:09 +0300

    Create http Book handler

--- app/src/main/java/isden/mois/magellanlauncher/httpd/HTTPD.java
+++ app/src/main/java/isden/mois/magellanlauncher/httpd/HTTPD.java
@@ -1,6 +1,8 @@
 package isden.mois.magellanlauncher.httpd;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
@@ -15,6 +17,7 @@ import java.util.Map;
 import fi.iki.elonen.router.RouterNanoHTTPD;
 import fi.iki.elonen.NanoHTTPD.Response.Status;
 import isden.mois.magellanlauncher.Constants;
+import isden.mois.magellanlauncher.httpd.handlers.BookHandler;
 import isden.mois.magellanlauncher.httpd.handlers.SQLHandler;
 import isden.mois.magellanlauncher.httpd.handlers.StaticHandler;
 import isden.mois.magellanlauncher.httpd.queries.SQLQuery;
@@ -29,11 +32,14 @@ public class HTTPD extends RouterNanoHTTPD {
 
     private File root;
     private Context ctx;
+    private String destination;
 
     public HTTPD(Context context) {
         super(PORT);
         this.ctx = context;
         this.root = context.getFilesDir().getAbsoluteFile();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        this.destination = prefs.getString("library_path", "/flash/Books");
 
         this.addMappings();
     }
@@ -45,14 +51,20 @@ public class HTTPD extends RouterNanoHTTPD {
         addRoute("/", StaticHandler.class, this.root);
         addRoute("/api/sql", SQLHandler.class, this.ctx);
         addRoute("/api/sql/:table", SQLHandler.class, this.ctx);
+        addRoute("/api/book", BookHandler.class, this.ctx, destination);
+        addRoute("/api/book/:MD5", BookHandler.class, this.ctx, destination);
+
         addRoute("/public/(.)+", StaticHandler.class, this.root);
-        addRoute("/(.)+", StaticHandler.class, this.root);
     }
 
     public static Response badRequest(String error) {
         JSONObject obj = new JSONObject();
         obj.put("error", error);
         return newFixedLengthResponse(Status.BAD_REQUEST, Constants.JSON, obj.toString());
+    }
+
+    public static Response success() {
+        return newFixedLengthResponse(Status.OK, Constants.JSON, Constants.SUCCESS_MESSAGE);
     }
 
     public static Response serveSQL(SQLQuery query, RouterNanoHTTPD.UriResource uriResource) {
