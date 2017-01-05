919e13f7798bd89e52b30800553f95c759f887cb
D. Moiseev <d.moiseev@initlab.ru>
2017-01-05 15:53:50 +0300

    Create SQLGet query

--- app/src/main/java/isden/mois/magellanlauncher/httpd/HTTPD.java
+++ app/src/main/java/isden/mois/magellanlauncher/httpd/HTTPD.java
@@ -2,11 +2,16 @@ package isden.mois.magellanlauncher.httpd;
 
 import android.content.Context;
 
+import com.alibaba.fastjson.JSONObject;
+
 import java.io.File;
 
 import fi.iki.elonen.router.RouterNanoHTTPD;
+import fi.iki.elonen.NanoHTTPD.Response.Status;
+import isden.mois.magellanlauncher.Constants;
 import isden.mois.magellanlauncher.httpd.handlers.SQLHandler;
 import isden.mois.magellanlauncher.httpd.handlers.StaticHandler;
+import isden.mois.magellanlauncher.httpd.queries.SQLQuery;
 
 /**
  * Created by isden on 05.01.17.
@@ -15,9 +20,11 @@ import isden.mois.magellanlauncher.httpd.handlers.StaticHandler;
 public class HTTPD extends RouterNanoHTTPD {
     private final static int PORT = 8080;
     private File root;
+    private Context ctx;
 
     public HTTPD(Context context) {
         super(PORT);
+        this.ctx = context;
         this.root = context.getFilesDir().getAbsoluteFile();
 
         this.addMappings();
@@ -28,8 +35,21 @@ public class HTTPD extends RouterNanoHTTPD {
         setNotFoundHandler(Error404UriHandler.class);
 
         addRoute("/", StaticHandler.class, this.root);
-        addRoute("/api/sql", SQLHandler.class);
+        addRoute("/api/sql", SQLHandler.class, this.ctx);
         addRoute("/public/(.)+", StaticHandler.class, this.root);
         addRoute("/(.)+", StaticHandler.class, this.root);
+    }
+
+    public static Response serveSQL(SQLQuery query, RouterNanoHTTPD.UriResource uriResource) {
+        Context ctx = uriResource.initParameter(Context.class);
+
+        try {
+            String result = query.execute(ctx);
+            return newFixedLengthResponse(Status.OK, Constants.JSON, result);
+        } catch (Exception e) {
+            JSONObject obj = new JSONObject();
+            obj.put("error", e.getMessage());
+            return newFixedLengthResponse(Status.BAD_REQUEST, Constants.JSON, obj.toString());
+        }
     }
 }
---
+++ app/src/main/java/isden/mois/magellanlauncher/httpd/queries/SQLQuery.java
@@ -0,0 +1,69 @@
+package isden.mois.magellanlauncher.httpd.queries;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import isden.mois.magellanlauncher.helpers.DBBooks;
+
+/**
+ * Created by isden on 05.01.17.
+ */
+
+abstract public class SQLQuery {
+    protected String[] params;
+
+    public String execute(Context context) throws Exception {
+        DBBooks dbBooks = new DBBooks(context);
+        try {
+            String query = buildQuery();
+            if (query == null || query.equals("")) {
+                return "[]";
+            }
+
+            SQLiteDatabase db = dbBooks.getReadableDatabase();
+
+            try {
+                Cursor c = db.rawQuery(query, params);
+
+                if (c != null) {
+                    try {
+                        return processQuery(c);
+                    } finally {
+                        c.close();
+                    }
+                }
+            } finally {
+                db.close();
+            }
+        } finally {
+            dbBooks.close();
+        }
+
+        return "[]";
+    }
+
+    protected String arrayResult(Cursor c) {
+        JSONArray result = new JSONArray();
+        String[] columns = c.getColumnNames();
+        int columnCount = columns.length;
+        c.moveToFirst();
+
+        while (c.moveToNext()) {
+            JSONObject row = new JSONObject();
+            for (int i = 0; i < columnCount; i++) {
+                row.put(columns[i], c.getString(i));
+            }
+
+            result.add(row);
+        }
+
+        return result.toJSONString();
+    }
+
+    abstract protected String buildQuery() throws Exception;
+    abstract protected String processQuery(Cursor cursor) throws Exception;
+}
